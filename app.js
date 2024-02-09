417d37772b47528da07ac92c5f8c8287c23abdb7
IvanZScode <sayonbra@gmail.com>
2024-02-09 19:53:11 +0100

    Feat: list and delete tasks

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
+// Init configurations
+require('./configs/hbs.config');
+
 const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`)
+
+const routes = require('./configs/routes.config');
+app.use('/', routes);
+
 
 const port = 3000;
 app.listen(port, () => console.info(`Application running at port ${port}`));
